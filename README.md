74b7e2036d9aa3a1f2aa800cfb5ef372360bb79d
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2023-10-27 11:28:45 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project-Marcelo
+
+This is a project of a portfolio is made with Html, Css and Javascript.
---
+++ script.js
@@ -0,0 +1,40 @@
+let menuVisible = false ;
+
+function showHideMenu() {
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function selected(){
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
+
+function effectSkills(){
+    let skills = document.getElementById("skills");
+    let distance_skills = window.innerHeight  - skills.getBoundingClientRect().top;
+    if(distance_skills >= 300){
+        let abilitys = document.getElementsByClassName("progress");
+        abilitys[0].classList.add("Html");
+        abilitys[1].classList.add("Css");
+        abilitys[2].classList.add("JavaScript");
+        abilitys[3].classList.add("Git");
+        abilitys[4].classList.add("Github");
+        abilitys[5].classList.add("React");
+        abilitys[6].classList.add("Comunication");
+        abilitys[7].classList.add("Creativity");
+        abilitys[8].classList.add("Teamwork");
+        abilitys[9].classList.add("Autodidact");
+        abilitys[10].classList.add("Leadership");
+        abilitys[11].classList.add("Adaptability");
+    }
+}
+
+window.onscroll = function(){
+    effectSkills();
+}
